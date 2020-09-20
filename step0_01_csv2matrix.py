042ff8e1d7e4827cacf4f86370c0bc926fdae3a5
Yuting_Zhao <47209089+mari-G@users.noreply.github.com>
2020-09-20 19:28:24 +0800

    Update step0_01_csv2matrix.py

--- step0_01_csv2matrix.py
+++ step0_01_csv2matrix.py
@@ -15,11 +15,9 @@ import pandas as pd
 from pandas import DataFrame
 
 matrix_df = pd.read_table("GSE72833_GeneExpressionTPM.txt", index_col= 0, header= 0, sep="\t", usecols=[0])
-
 # print(matrix_df)
 # print(matrix_df.index.is_unique) # True
 # print(matrix_df)
-
 
 def csv2pd(csv_file):
     global csv_df
@@ -36,6 +34,18 @@ def csv2pd(csv_file):
 for csv_file in total_csv_list:
     csv2pd(csv_file)
 
+
+matrix_df.dropna(axis=0, how="all", inplace=True)
 matrix_df = matrix_df.fillna(0)
+# matrix_df.iloc[~(matrix_df == 0).all(axis= 1), :] fail
 # print(matrix_df)
 matrix_df.to_csv("matrix.csv")
+
+def classification_csv():
+    classification_df = pd.DataFrame(index= total_csv_list)
+    classification_df["sample"] = total_csv_list
+    classification_df["day_stage"] = total_csv_list
+    classification_df["stage"] = classification_df["day_stage"].map(lambda x: x.split(r"_")[1])
+    classification_df.to_csv("classification.csv")
+
+classification_csv()
